ed97861ce1a87f3c73d5a0b43dc03c202969fa6d
Jeff Whitaker <jswhit@fastmail.fm>
2015-09-28 10:57:41 -0600

    cleanup

--- KS.py
+++ KS.py
@@ -33,57 +33,24 @@ class KS(object):
         self.ik    = 1j*k              # spectral derivative operator
         self.lin   = k**2 - diffusion*(k**exponent)  # Fourier multipliers for linear term
         # random noise initial condition.
-        u = 0.01*np.random.standard_normal(size=(members,N))
+        x = 0.01*np.random.standard_normal(size=(members,N))
         # remove zonal mean from initial condition.
-        u = u - u.mean()
+        self.x = x - x.mean()
         # spectral space variable
-        self.xspec = np.fft.rfft(u)
-        self.x = u
-    def nlterm(self,v):
+        self.xspec = np.fft.rfft(self.x,axis=-1)
+    def nlterm(self,xspec):
         # compute tendency from nonlinear term.
-        u = np.fft.irfft(v,axis=-1)
-        return -0.5*self.ik*np.fft.rfft(u**2,axis=-1)
+        x = np.fft.irfft(xspec,axis=-1)
+        return -0.5*self.ik*np.fft.rfft(x**2,axis=-1)
     def advance(self):
         # semi-implicit third-order runge kutta update.
         # ref: http://journals.ametsoc.org/doi/pdf/10.1175/MWR3214.1
         self.xspec = np.fft.rfft(self.x,axis=-1)
-        vsave = self.xspec.copy()
-        v = self.xspec.copy()
+        xspec_save = self.xspec.copy()
         for n in range(3):
             dt = self.dt/(3-n)
             # explicit RK3 step for nonlinear term
-            v = vsave + dt*self.nlterm(v)
+            self.xspec = xspec_save + dt*self.nlterm(self.xspec)
             # implicit trapezoidal adjustment for linear term
-            v = (v+0.5*self.lin*dt*vsave)/(1.-0.5*self.lin*dt)
-        self.xspec = v
-        self.x = np.fft.irfft(v,axis=-1)
-        # Lorenz 3-cycle + trapezoidal
-        # ref: http://dx.doi.org/10.1175/MWR-D-13-00132.1
-
-        #a21 = 1./3.
-        #a31 = 1./6.; a32 = 1./2. # scheme 1
-        #b1 = 0.5; b2 = -0.5; b3 = 1. # scheme 1
-        ##a31 = -1./3.; a32 = 1. # scheme 2
-        ##b1 = 0.; b2 = 0.5; b3 = 0.5 # scheme 2
-        #aa21 = 1./6.; aa22 = 1./6.
-        #aa31 = 1./3;  aa32 = 0.; aa33 = 1./3.
-        #b = a31+a32
-        #c = 1./5.
-        #d = (1./b)*(0.5-c)
-        #bb1 = 1-(c+d); bb2 = 0.; bb3 = d; bb4 = c
-
-        ## stage 1
-        #nlterm1 = dt*self.nlterm(v1)
-        #linterm = dt*self.lin
-        #v2 = v1 + a21*nlterm1 + aa21*v1*linterm
-        #v2 = v2/(1.-aa22*linterm)
-        ## stage 2
-        #nlterm2 = dt*self.nlterm(v2)
-        #v3 = v1 + a31*nlterm1 + a32*nlterm2 +\
-        #          aa31*v1*linterm + aa32*v2*linterm
-        #v3 = v3/(1.-aa33*linterm)
-        ## stage 3
-        #nlterm3 = dt*self.nlterm(v3)
-        #v = v1 + nlterm1*b1 + nlterm2*b2 + nlterm3*b3 +\
-        #         bb1*linterm*v1 + bb2*linterm*v2 + bb3*linterm*v3
-        #v = v/(1.-bb4*linterm)
+            self.xspec = (self.xspec+0.5*self.lin*dt*xspec_save)/(1.-0.5*self.lin*dt)
+        self.x = np.fft.irfft(self.xspec,axis=-1)
