3895ca480f8ce2bb2a99ae95f3d7275587e89a4c
Jeff Whitaker <jswhit@fastmail.fm>
2016-11-10 07:31:07 -0700

    zonally varying diffusion

--- KS.py
+++ KS.py
@@ -19,12 +19,14 @@ class KS(object):
         kk = N*np.fft.fftfreq(N)[0:(N/2)+1]  # wave numbers
         self.wavenums = kk
         k  = kk.astype(np.float)/L
+        self.k     = k
         self.ik    = 1j*k                   # spectral derivative operator
         self.lin   = k**2 - diffusion*k**4  # Fourier multipliers for linear term
         # random noise initial condition.
         if rs is None:
             rs = np.random.RandomState()
         x = 0.01*rs.standard_normal(size=(members,N))
+        self.extradiff = 2.0*diffusion*np.cos(np.linspace(0,np.pi,N))**4
         # remove zonal mean from initial condition.
         self.x = x - x.mean()
         # spectral space variable
@@ -32,7 +34,8 @@ class KS(object):
     def nlterm(self,xspec):
         # compute tendency from nonlinear term.
         x = np.fft.irfft(xspec,axis=-1)
-        return -0.5*self.ik*np.fft.rfft(x**2,axis=-1)
+        extradiff = -self.extradiff*np.fft.irfft(self.k**2*xspec,axis=-1)
+        return -0.5*self.ik*np.fft.rfft(x**2+extradiff,axis=-1)
     def advance(self):
         # semi-implicit third-order runge kutta update.
         # ref: http://journals.ametsoc.org/doi/pdf/10.1175/MWR3214.1
--- KSanimate.py
+++ KSanimate.py
@@ -1,11 +1,11 @@
 import numpy as np
-from KS import KS
+from KSvaryingdiff import KS
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-L   = 16           # domain is 0 to 2.*np.pi*L
-N   = 128          # number of collocation points
-dt  = 0.5          # time step
+L   = 32           # domain is 0 to 2.*np.pi*L
+N   = 512          # number of collocation points
+dt  = 0.3          # time step
 diffusion = 1.0
 ks = KS(L=L,diffusion=diffusion,N=N,dt=dt) # instantiate model
 
@@ -18,14 +18,14 @@ u = u - u.mean()
 ks.xspec[0] = np.fft.rfft(u)
 
 # time stepping loop.
-nmin = 1000; nmax = 5000
+nmin = 400; nmax = 2000
 uu = []; tt = []
 vspec = np.zeros(ks.xspec.shape[1], np.float)
 x = np.arange(N)
 fig, ax = plt.subplots()
 line, = ax.plot(x, ks.x.squeeze())
 ax.set_xlim(0,N-1)
-ax.set_ylim(-3,3)
+ax.set_ylim(-10,10)
 #Init only required for blitting to give a clean slate.
 def init():
     global line
@@ -46,8 +46,11 @@ def updatefig(n):
     uu.append(u); tt.append(n*dt)
     return line,
 
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 ani = animation.FuncAnimation(fig, updatefig, np.arange(1,nmax+1), init_func=init,
                               interval=25, blit=True, repeat=False)
+ani.save('KS.mp4',writer=writer)
 plt.show()
 
 plt.figure()
@@ -56,17 +59,26 @@ ncount = len(uu)
 vspec = vspec/ncount
 uu = np.array(uu); tt = np.array(tt)
 print tt.min(), tt.max()
-nplt = 200
+print uu.shape
+uup = uu - uu.mean(axis=0)
+print uup.shape
+cov = np.dot(uup.T,uup)
+print 'cov',cov.min(), cov.max(), cov.shape
+nplt = 800
 plt.contourf(x,tt[:nplt],uu[:nplt],31,cmap=plt.cm.spectral,extend='both')
 plt.xlabel('x')
 plt.ylabel('t')
 plt.colorbar()
 plt.title('chaotic solution of the K-S equation')
 
+
 plt.figure()
 plt.loglog(ks.wavenums,vspec)
 plt.title('variance spectrum')
-plt.ylim(0.001,10000)
-plt.xlim(0,100)
+#plt.ylim(0.001,10000)
+#plt.xlim(0,100)
+
+plt.figure()
+plt.contourf(x,x,cov,31,cmap=plt.cm.spectral,extend='both')
 
 plt.show()
